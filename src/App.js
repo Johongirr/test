b8f535c108f24a0743f8f6e7810e12f14982a1d6
Johongirr <johongir_rahimov@bk.ru>
2022-04-16 12:17:10 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Table from "./components/Table/Table";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import axios from "axios";
+import { getUser, removeUser, setUser } from "./storage/storage";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(getUser());
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  const getAllUsers = () => {
+    axios
+      .get("http://localhost:3001/api/user/getAllUsers")
+      .then((res) => {
+        setUsers(res.data);
+        const isCurrentUserExist = currentUser
+          ? res.data.some((user) => user._id === currentUser?.id)
+          : true;
+        if (!isCurrentUserExist) {
+          alert("Removing");
+          removeUser();
+          setCurrentUser(null);
+        }
+        const isCurrentUserBlocked = res.data.some(
+          (user) => user._id === currentUser?.id && user.isBlocked
+        );
+        if (isCurrentUserBlocked) {
+          currentUser.isBlocked = true;
+          setUser(currentUser);
+          setCurrentUser(currentUser);
+        }
+        const isCurrentUserUnblocked = res.data.some(
+          (user) => user._id === currentUser?.id && !user.isBlocked
+        );
+        if (isCurrentUserUnblocked) {
+          currentUser.isBlocked = false;
+          setUser(currentUser);
+          setCurrentUser(currentUser);
+        }
+        console.log(isCurrentUserBlocked, res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const blockUser = (id) => {
+    axios
+      .post("http://localhost:3001/api/user/blockUser", { id })
+      .then((res) => {
+        if (currentUser.id === id) {
+          currentUser.isBlocked = true;
+          setUser(currentUser);
+          updateCurrentUser(currentUser);
+        }
+        getAllUsers();
+      });
+  };
+  const unblockUser = (id) => {
+    axios
+      .post("http://localhost:3001/api/user/unblockUser", { id })
+      .then((res) => {
+        if (currentUser.id === id) {
+          currentUser.isBlocked = false;
+          setUser(currentUser);
+          updateCurrentUser(currentUser);
+        }
+        getAllUsers();
+      });
+  };
+  const deleteUser = (id) => {
+    axios
+      .post("http://localhost:3001/api/user/deleteUser", { id })
+      .then((res) => {
+        if (currentUser.id === id) {
+          removeUser();
+          updateCurrentUser(null);
+        }
+        getAllUsers();
+      });
+  };
+  const updateCurrentUser = (currentUser) => setCurrentUser(currentUser);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={
+          <div className="container mt-5">
+            <h1>Welcome {currentUser?.username}</h1>
+            <Table
+              users={users}
+              currentUser={currentUser}
+              blockUser={blockUser}
+              unblockUser={unblockUser}
+              deleteUser={deleteUser}
+            />
+          </div>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <Register
+            currentUser={currentUser}
+            updateCurrentUser={updateCurrentUser}
+          />
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <Login
+            currentUser={currentUser}
+            updateCurrentUser={updateCurrentUser}
+            getAllUsers={getAllUsers}
+            users={users}
+          />
+        }
+      />
+    </Routes>
   );
 }
 
